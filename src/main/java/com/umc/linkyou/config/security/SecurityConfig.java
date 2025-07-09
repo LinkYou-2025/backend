641cd4abfa9c7393949557b6acd5e80099d0efd5
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2025-07-09 14:29:42 +0900

    feat: #11 Swagger 설정 및 예시 testcontroller 작성

--- src/main/java/com/umc/linkyou/config/security/SecurityConfig.java
+++ src/main/java/com/umc/linkyou/config/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/css/**", "/users/**", "/users/join", "/users/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/", "/css/**", "/users/**", "/users/join", "/users/login", "/api/test/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
