3da185a4263f1d9afb4d7bb0697216ed781ed4e8
HyeonJooooo <la0503ab@gmail.com>
2025-07-18 21:31:15 +0900

    :bug: 큐레이션 관련 bug fix

--- src/main/java/com/umc/linkyou/repository/SituationJobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umc.linkyou.repository;
-
-import com.umc.linkyou.domain.mapping.SituationJob;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SituationJobRepository extends JpaRepository<SituationJob, Long> {
-}
