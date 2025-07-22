4894b951b99463c3378442715b280a456b5c7b4b
Hajin99 <hajin6529@naver.com>
2025-07-22 21:04:56 +0900

    #69 feat: 마이페이지에 이메일, 성별, 직업 정보 추가

--- src/main/java/com/umc/linkyou/converter/UserConverter.java
+++ src/main/java/com/umc/linkyou/converter/UserConverter.java
@@ -42,9 +42,12 @@ public class UserConverter {
                 .build();
     }
 
-    public static UserResponseDTO.UserInfoDTO toUserInfoDTO(String nickName, Long linkCount, Long folderCount, Long aiLinkCount) {
+    public static UserResponseDTO.UserInfoDTO toUserInfoDTO(String nickName, String email, Gender gender, Job job, Long linkCount, Long folderCount, Long aiLinkCount) {
         return UserResponseDTO.UserInfoDTO.builder()
                 .nickname(nickName)
+                .email(email)
+                .gender(gender)
+                .job(job)
                 .myLinku(linkCount)
                 .myFolder(folderCount)
                 .myAiLinku(aiLinkCount)
--- src/main/java/com/umc/linkyou/service/UserServiceImpl.java
+++ src/main/java/com/umc/linkyou/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.umc.linkyou.apiPayload.exception.handler.UserHandler;
 import com.umc.linkyou.config.security.jwt.JwtTokenProvider;
 import com.umc.linkyou.converter.UserConverter;
 import com.umc.linkyou.domain.EmailVerification;
+import com.umc.linkyou.domain.enums.Gender;
 import com.umc.linkyou.domain.folder.Folder;
 import com.umc.linkyou.domain.classification.Category;
 import com.umc.linkyou.domain.classification.Interests;
@@ -244,12 +245,15 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDTO.UserInfoDTO userInfo(Long userId){
         String nickName = userQueryRepository.findNicknameByUserId(userId);
+        String email = userQueryRepository.findEmailByUserId(userId);
+        Gender gender = userQueryRepository.findGenderByUserId(userId);
+        Job job = userQueryRepository.findJobByUserId(userId);
         Long linkCount = userQueryRepository.countLinksByUserId(userId);
         Long folderCount = userQueryRepository.countFoldersByUserId(userId);
         Long aiLinkCount = userQueryRepository.countAiLinksByUserId(userId);
 
         return UserConverter.toUserInfoDTO(
-                nickName, linkCount, folderCount, aiLinkCount
+                nickName, email, gender, job, linkCount, folderCount, aiLinkCount
         );
     }
 
