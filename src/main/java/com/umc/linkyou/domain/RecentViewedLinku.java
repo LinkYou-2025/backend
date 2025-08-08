649ddeb46d8beb2f1ad1bda84f8b1846c0b188ca
oculo0204 <oculo0204@gmail.com>
2025-08-08 22:40:21 +0900

    !hotfix: users_id 오류

--- src/main/java/com/umc/linkyou/domain/RecentViewedLinku.java
+++ src/main/java/com/umc/linkyou/domain/RecentViewedLinku.java
@@ -19,7 +19,7 @@ public class RecentViewedLinku {
     private Long recentViewedLinkuId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "users_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 
     @ManyToOne(fetch = FetchType.LAZY)
